89f81e4b2d8b3712b01c3f14130f841c86a25310
Jflynn426 <jflynnpics@gmail.com>
2020-02-26 13:56:50 -0500

    Database created and linked! Models folder imported

---
+++ Painting website/Models/Picture.cs
@@ -0,0 +1,20 @@
+﻿namespace Capstone.Models
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        public string SpeciesName { get; set; }
+
+        public string PictureInfo { get; set; }
+
+        public Location Location { get; set; }
+        public int? LocationId { get; set; }
+        public int? TagId { get; set; }
+        public Tag Tag { get; set; }
+
+        public string ImageUrl { get; set; }
+
+    }
+
+
+}
